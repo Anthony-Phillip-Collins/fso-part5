bf1d4b5ebc978b18fbf1f82091950033461317f4
Anthony-Phillip-Collins <anthony.phillip.collins@gmail.com>
2022-10-10 13:07:13 +1000

    5.7* Blog list frontend, step7

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ const App = () => {
         </div>
 
         {blogs.map((blog) => (
-          <Blog key={blog.id} blog={blog} />
+          <Blog key={blog.id} {...blog} />
         ))}
       </>
     );
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = (props) => {
+  const [expand, setExpand] = useState(false);
+  const { title, author, url, likes, user } = props;
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggle = () => setExpand(!expand);
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {title} <b>{author}</b>
+        <button onClick={toggle}>{expand ? 'hide' : 'view'}</button>
+      </div>
+
+      {expand && (
+        <>
+          <div>{url}</div>
+          <div>likes {likes}</div>
+          <div>{user.name}</div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
