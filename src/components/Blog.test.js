98899533e6d1586111893aea74d944d2ac64a13b
Anthony-Phillip-Collins <anthony.phillip.collins@gmail.com>
2022-10-12 13:50:02 +1000

    5.15: Blog list tests, step3

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { fireEvent, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { prettyDOM } from '@testing-library/dom';
 import Blog from './Blog';
 
@@ -42,15 +41,30 @@ describe('Blog', () => {
   test('shows url and likes when toggle button is clicked', () => {
     render(<Blog blog={blog} onDelete={() => {}} onUpdate={() => {}} />);
 
-    const button = screen.getByRole('button', { name: 'view' });
-    fireEvent.click(button);
+    const toggleButton = screen.getByRole('button', { name: 'view' });
+    fireEvent.click(toggleButton);
 
-    expect(button).toHaveTextContent('hide');
+    expect(toggleButton).toHaveTextContent('hide');
 
     const url = screen.getByText('https://thesimpsons.com');
     expect(url).toBeDefined();
 
     const likes = screen.getByText('likes', { exact: false });
     expect(likes).toBeDefined();
+  });
+
+  test('like button is clicked twice and eventHandler is called twice', () => {
+    const onUpdate = jest.fn();
+
+    render(<Blog blog={blog} onDelete={() => {}} onUpdate={onUpdate} />);
+
+    const toggleButton = screen.getByRole('button', { name: 'view' });
+    fireEvent.click(toggleButton);
+
+    const likeButton = screen.getByRole('button', { name: 'like' });
+    fireEvent.click(likeButton);
+    fireEvent.click(likeButton);
+
+    expect(onUpdate).toBeCalledTimes(2);
   });
 });
