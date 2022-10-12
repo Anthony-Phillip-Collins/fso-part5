2efb717689b83a500b9d662c09f06fa38938eaf7
Anthony-Phillip-Collins <anthony.phillip.collins@gmail.com>
2022-10-12 13:41:16 +1000

    5.14: Blog list tests, step2

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen } from '@testing-library/react';
-// import { prettyDOM } from '@testing-library/dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { prettyDOM } from '@testing-library/dom';
 import Blog from './Blog';
 
-describe('Blog', () => {
-  test('initially renders title and author, but not the url or likes', async () => {
-    const blog = {
-      title: 'Springfield News',
-      author: 'Homer Simpson',
-      url: 'https://thesimpsons.com',
-      likes: 0,
-      id: '0',
-      user: {
-        name: 'User 1',
-        username: 'User1',
-        id: '0',
-      },
-    };
+let blog;
 
+beforeAll(() => {
+  blog = {
+    title: 'Springfield News',
+    author: 'Homer Simpson',
+    url: 'https://thesimpsons.com',
+    likes: 0,
+    id: '0',
+    user: {
+      name: 'User 1',
+      username: 'User1',
+      id: '0',
+    },
+  };
+});
+
+describe('Blog', () => {
+  test('initially renders title and author, but not the url or likes', () => {
     render(<Blog blog={blog} onDelete={() => {}} onUpdate={() => {}} />);
 
     const title = screen.getByText('Springfield News');
@@ -32,5 +37,20 @@ describe('Blog', () => {
 
     const likes = screen.queryByText('likes', { exact: false });
     expect(likes).toBeNull();
+  });
+
+  test('shows url and likes when toggle button is clicked', () => {
+    render(<Blog blog={blog} onDelete={() => {}} onUpdate={() => {}} />);
+
+    const button = screen.getByRole('button', { name: 'view' });
+    fireEvent.click(button);
+
+    expect(button).toHaveTextContent('hide');
+
+    const url = screen.getByText('https://thesimpsons.com');
+    expect(url).toBeDefined();
+
+    const likes = screen.getByText('likes', { exact: false });
+    expect(likes).toBeDefined();
   });
 });
